c9071bda2504d1e6d3da935a110fd0e35e38b847
simon <simonzhang0428@gmail.com>
2021-06-27 11:55:40 -0700

    704_BinarySearch

---
+++ BinarySearch704.java
@@ -0,0 +1,25 @@
+class BinarySearch704 {
+    public static int binarySearch(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right - 1) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target)
+                return mid;
+            else if (nums[mid] < target)
+                left = mid;
+            else
+                right = mid;
+        }
+        if (nums[left] == target)
+            return left;
+        if (nums[right] == target)
+            return right;
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 1, 3, 5, 7, 9 };
+        System.out.println(binarySearch(nums, 5));
+    }
+}
