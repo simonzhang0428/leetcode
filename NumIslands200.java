8c71d3ab29915fc7e8c4565948506e75cf7fbedb
simon <simonzhang0428@gmail.com>
2021-06-30 10:28:41 -0700

    200_number of islands

---
+++ NumIslands200.java
@@ -0,0 +1,93 @@
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+class Solution200 {
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+        int ans = 0;
+        final int[][] DIRS = { { 0, 1 }, { 0, -1 }, { 1, 0 }, { -1, 0 } };
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1') {
+                    dfs(grid, i, j, DIRS);
+                    ans++;
+                }
+            }
+        }
+
+        return ans;
+    }
+
+    private void dfs(char[][] grid, int x, int y, int[][] DIRS) {
+        if (!isValid(grid, x, y) || grid[x][y] == '0') {
+            return;
+        }
+
+        grid[x][y] = '0';
+        for (int[] direction : DIRS) {
+            int nextX = x + direction[0];
+            int nextY = y + direction[1];
+            dfs(grid, nextX, nextY, DIRS);
+        }
+    }
+
+    private boolean isValid(char[][] grid, int x, int y) {
+        if (x < 0 || x >= grid.length || y < 0 || y >= grid[0].length) {
+            return false;
+        }
+        return true;
+    }
+
+    // bfs
+    public int numIslands_bfs(char[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+        final int[][] DIR = { { 0, 1 }, { 0, -1 }, { -1, 0 }, { 1, 0 } };
+        int count = 0;
+        Queue<int[]> queue = new ArrayDeque<>();
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1') {
+                    bfs(grid, i, j, DIR, queue);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    private void bfs(char[][] grid, int x, int y, int[][] DIR, Queue<int[]> queue) {
+        queue.offer(new int[] { x, y });
+        grid[x][y] = '0';
+        while (!queue.isEmpty()) {
+            int[] cur = queue.poll();
+            for (int[] direction : DIR) {
+                int nextX = cur[0] + direction[0];
+                int nextY = cur[1] + direction[1];
+                if (isValid(grid, nextX, nextY) && grid[nextX][nextY] == '1') {
+                    queue.offer(new int[] { nextX, nextY });
+                    grid[nextX][nextY] = '0';
+                }
+            }
+        }
+    }
+
+}
+
+public class NumIslands200 {
+    
+    public static void main(String[] args) {
+        char[][] grid = {
+            {'1', '1', '0', '0', '0'},
+            {'1', '1', '0', '0', '0'},
+            {'0', '0', '1', '0', '0'},
+            {'0', '0', '0', '1', '1'}
+        };
+        int ret = new Solution200().numIslands_bfs(grid);
+        System.out.println(ret);
+    }
+}
