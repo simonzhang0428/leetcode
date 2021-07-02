03a723935c451b357d60a796271bd43f276a6a59
simon <simonzhang0428@gmail.com>
2021-07-01 22:03:49 -0700

    wall and gate

---
+++ WallAndGate286.java
@@ -0,0 +1,56 @@
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+class Solution286 {
+    /**
+     * @param rooms: m x n 2D grid
+     * @return: nothing
+     */
+    public void wallsAndGates(int[][] rooms) {
+        // write your code here
+        int m = rooms.length;
+        int n = rooms[0].length;
+        final int[][] DIRS = { { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } };
+        Queue<int[]> queue = new ArrayDeque<>();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (rooms[i][j] == 0) {
+                    queue.offer(new int[] { i, j });
+                }
+            }
+        }
+        while (!queue.isEmpty()) {
+            int[] tmp = queue.poll();
+            int x = tmp[0];
+            int y = tmp[1];
+            if (rooms[x][y] == -1) {
+                continue;
+            }
+            for (int i = 0; i < 4; i++) {
+                int newX = x + DIRS[i][0];
+                int newY = y + DIRS[i][1];
+                if (newX >= 0 && newX < m && newY >= 0 && newY < n && rooms[newX][newY] == Integer.MAX_VALUE) {
+                    rooms[newX][newY] = rooms[x][y] + 1;
+                    queue.offer(new int[] { newX, newY });
+                }
+            }
+        }
+    }
+}
+public class WallAndGate286 {
+    public static void main(String[] args) {
+        int[][] rooms = {
+            {Integer.MAX_VALUE, -1, 0, Integer.MAX_VALUE},
+            {Integer.MAX_VALUE, Integer.MAX_VALUE, Integer.MAX_VALUE, -1},
+            {Integer.MAX_VALUE, -1, Integer.MAX_VALUE, -1},
+            {0, -1, Integer.MAX_VALUE, Integer.MAX_VALUE}
+        };
+        new Solution286().wallsAndGates(rooms);
+        for (int i = 0; i < rooms.length; i++) {
+            for (int j = 0; j < rooms[0].length; j++) {
+                System.out.print(rooms[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
