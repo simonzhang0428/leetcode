f2d4fd3c440ea939cf52fb2bc0ee8b543bb76f96
simon <simonzhang0428@gmail.com>
2021-07-06 21:30:47 -0700

    2D -> 1D

--- Maze490.java
+++ Maze490.java
@@ -3,29 +3,39 @@ import java.util.Queue;
 
 class Solution490 {
     public boolean hasPath(int[][] maze, int[] start, int[] destination) {
-        boolean[][] visited = new boolean[maze.length][maze[0].length];
-        final int[][] DIRS = { { 0, 1 }, { 0, -1 }, { -1, 0 }, { 1, 0 } };
-        Queue<int[]> queue = new ArrayDeque<>();
-        queue.offer(start);
+        // write your code here
+        int m = maze.length, n = maze[0].length;
+        boolean[][] visited = new boolean[m][n];
+
+        Queue<Integer> queue = new ArrayDeque<>();
+
+        queue.offer(start[0] * n + start[1]);
         visited[start[0]][start[1]] = true;
+
+        int[] dirs = { -1, 0, 1, 0, -1 };
         while (!queue.isEmpty()) {
-            int[] s = queue.poll();
-            if (s[0] == destination[0] && s[1] == destination[1]) {
-                return true;
-            }
-            for (int[] dir : DIRS) {
-                int x = s[0] + dir[0];
-                int y = s[1] + dir[1];
-                while (x >= 0 && y >= 0 && x < maze.length && y < maze[0].length && maze[x][y] == 0) {
-                    x += dir[0];
-                    y += dir[1];
+            int cur = queue.poll();
+
+            for (int i = 0; i < 4; i++) {
+                int x = cur / n, y = cur % n;
+                while (x >= 0 && x < m && y >= 0 && y < n && maze[x][y] != 1) {
+                    x += dirs[i];
+                    y += dirs[i + 1];
                 }
-                if (!visited[x - dir[0]][y - dir[1]]) {
-                    queue.offer(new int[] { x - dir[0], y - dir[1] });
-                    visited[x - dir[0]][y - dir[1]] = true;
+                x -= dirs[i];
+                y -= dirs[i + 1];
+
+                if (x == destination[0] && y == destination[1]) {
+                    return true;
+                }
+
+                if (!visited[x][y]) {
+                    queue.offer(x * n + y);
+                    visited[x][y] = true;
                 }
             }
         }
+
         return false;
     }
 }
