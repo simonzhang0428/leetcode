b1fa54bf29522943a3a1c9c2f40c93812da60533
simon <simonzhang0428@gmail.com>
2021-07-14 11:13:40 -0700

    reduce space complexity

--- MaxSubArray53.java
+++ MaxSubArray53.java
@@ -33,11 +33,34 @@ class Solution53 {
 
         return globalMax;
     }
+
+
+    // reduce space complexity, only use prev and curr
+    public int maxSubArray2(int[] nums) {
+        int n = nums.length;
+        if (n == 1) {
+            return nums[0];
+        }
+
+        int prev = nums[0];
+        int globalMax = nums[0];
+        for (int i = 1; i < n; i++) {
+            int curr = nums[i];
+            if (prev >= 0) {
+                curr += prev;
+            } 
+            globalMax = Math.max(globalMax, curr);
+            prev = curr;
+        }
+
+        return globalMax;
+    }
 }
 
 public class MaxSubArray53 {
     public static void main(String[] args) {
         int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
         System.out.println("6:  " + new Solution53().maxSubArray(nums));
+        System.out.println("6:  " + new Solution53().maxSubArray2(nums));
     }
 }
